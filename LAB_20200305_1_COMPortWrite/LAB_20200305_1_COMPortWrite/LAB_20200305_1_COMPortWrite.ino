70280fa9944f70a09bd47aaba143632027410658
TIK <firmament_solo@hotmail.com>
2020-03-05 17:17:39 +0800

    Added PIN2 control for LAB_20200305_1

--- LAB_20200305_1_COMPortWrite/LAB_20200305_1_COMPortWrite/LAB_20200305_1_COMPortWrite.ino
+++ LAB_20200305_1_COMPortWrite/LAB_20200305_1_COMPortWrite/LAB_20200305_1_COMPortWrite.ino
@@ -1,11 +1,16 @@
 //For LAB_20200305_1 COMPort write test(Arduino Receving)
 //-------------------------
+//MAR 05 2020 T.I. PIN2 control
 //MAR 03 2020 T.I. Created this file
+
+int incomingByte = 0; // for incoming serial data
 
 void setup() {
   // put your setup code here, to run once:
     Serial.setTimeout(50);
     Serial.begin(9600);
+    pinMode(2,OUTPUT);
+    digitalWrite(2,LOW); 
 
 }
 
@@ -15,7 +20,14 @@ void loop() {
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
-
+    if('A'==incomingByte)
+    {
+      digitalWrite(2,HIGH); 
+    }
+    else
+    {
+      digitalWrite(2,LOW); 
+    }
 
   }
 }
