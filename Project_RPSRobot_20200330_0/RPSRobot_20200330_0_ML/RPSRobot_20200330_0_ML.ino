fa99a4634c286db436da9011be5be7dedd779b24
TIK <firmament_solo@hotmail.com>
2020-04-08 16:30:32 +0800

    Added ANN version for RPSRobot project. Fixed incorrect matchscore table in RPS FW

--- Project_RPSRobot_20200330_0/RPSRobot_20200330_0_ML/RPSRobot_20200330_0_ML.ino
+++ Project_RPSRobot_20200330_0/RPSRobot_20200330_0_ML/RPSRobot_20200330_0_ML.ino
@@ -1,9 +1,7 @@
-//Project RPSRobot 20200401_0
+//Project RPSRobot Machine Learning version
 //================
-//Apr 8 2020 T.I. Fixed incorrect matchScore table 
-//Apr 7 2020 T.I. Added Win/Lose LED, SegdDisplay driver,Integration Test
-//Apr 2 2020 T.I. Added countdown, randomgesture, score rules
-//Apr 1 2020 T.I. created this file
+//Apr 8 2020 T.I. Added ANN engine
+//Based on Project RPSRobot github "3d7a842"
 
 
 #include <Servo.h>
@@ -48,6 +46,98 @@ int startButton=13;
 int pinLoseLED=7;
 int pinWinLED=A5;
 
+float ANNinputs[3]={0.0,0.0,0.0};
+
+float Layer0_W[4][4]={{-3.35796,-1.126993,0.00737043,1.9779116},
+                      {-2.4028397,-1.3854587,-0.7931496,2.080642},
+                      {1.0243597,2.1095006,1.6874604,-2.316983},
+                      {1.8379576,1.5571779,1.2571335,-2.14945}};
+                      
+float Layer0_Outputs[4]={0.0,0.0,0.0,0.0};
+
+float Layer1_W[4][5]={{3.109413,2.1515133,-1.7762052,-2.118841,1.3897169},
+                      {-0.54771924,-2.6526208,3.062316,2.4938545,0.12920415},
+                      {1.0274583,1.9144325,-3.6218135,-2.1290317,0.1106161},
+                      {-3.6488092,-3.56605,1.812533,1.8006505,-1.1013095}};
+                      
+float Layer1_Outputs[4]={0.0,0.0,0.0,0.0};
+
+float LayerOutputs_W[3][5]={{-3.0573914,-0.33590198,-2.9392657,3.5195374,-0.66786194},
+                            {1.2544978,2.7321491,-2.4654458,-39482572,-1.2988576},
+                            {1.7912973,-3.1768422,1.5621092,-1.8792809,-0.85067666}};
+                      
+float ANNOutputs[3]={0.0,0.0,0.0};
+
+void ANNRun()
+{
+  int i,j;
+
+  for(i=0;i<4;i++)
+  {
+    Layer0_Outputs[i]=0.0;
+    Layer1_Outputs[i]=0.0;
+  }
+  
+  for(i=0;i<3;i++)
+  {
+      ANNOutputs[i]=0.0;
+  }
+
+  
+  //Layer0
+  for(i=0;i<4;i++)
+  {
+    for(j=0;j<3;j++)
+    {
+      Layer0_Outputs[i]=Layer0_Outputs[i]+(ANNinputs[j]*Layer0_W[i][j]);
+
+    }
+    Layer0_Outputs[i]=Layer0_Outputs[i]+Layer0_W[i][3];
+    Layer0_Outputs[i]=HardSigmoid(Layer0_Outputs[i]);
+
+  }
+  //Layer1
+  for(i=0;i<4;i++)
+  {
+    for(j=0;j<4;j++)
+    {
+      Layer1_Outputs[i]=Layer1_Outputs[i]+(Layer0_Outputs[j]*Layer1_W[i][j]);
+    }
+    Layer1_Outputs[i]=Layer1_Outputs[i]+Layer1_W[i][4];
+    Layer1_Outputs[i]=HardSigmoid(Layer1_Outputs[i]);
+  }   
+  //LayerOutputs
+  for(i=0;i<3;i++)
+  {
+    for(j=0;j<4;j++)
+    {
+      ANNOutputs[i]=ANNOutputs[i]+(Layer1_Outputs[j]*LayerOutputs_W[i][j]);
+    }
+     ANNOutputs[i]= ANNOutputs[i]+LayerOutputs_W[i][4];
+     ANNOutputs[i]=HardSigmoid(ANNOutputs[i]);
+
+  }   
+
+}
+
+int GetANNMaxIndex()
+{
+  int i=0;
+  int MAXidx=0;
+  float MAXValue=0.0;
+
+  for(i=0;i<3;i++)
+  {
+    if(MAXValue<ANNOutputs[i])
+    {
+      MAXidx=i;
+      MAXValue=ANNOutputs[i];
+    }
+  }
+  
+
+  return MAXidx;
+}
 
 void setup() {
 
@@ -83,7 +173,6 @@ void loop() {
   int userGesture;
   int matchResult=0;
   int startButtonST=0;
-
 
   do
   {
@@ -145,8 +234,9 @@ void loop() {
         ADCVal[i]  = ADC_180_DEGREE;
       } 
   }
+
   
-  userGesture=gestureRecognize(ADCVal);
+  userGesture=gestureRecognizeML(ADCVal);
 
 
 
@@ -217,6 +307,31 @@ int gestureRecognize(int *pFingerADC)
   return rt;
 }
 
+int gestureRecognizeML(int *pFingerADC)
+{
+  int rt=0;
+
+  Serial.print("0:");
+  Serial.print(pFingerADC[0]);
+  Serial.print("\n");
+  Serial.print("1:");
+  Serial.print(pFingerADC[1]);
+  Serial.print("\n");
+  Serial.print("2:");
+  Serial.print(pFingerADC[2]);
+  Serial.print("\n");
+
+
+  ANNinputs[0]=normalizeML((float)pFingerADC[0],240.0,208.0);
+  ANNinputs[1]=normalizeML((float)pFingerADC[1],220.0,208.0);
+  ANNinputs[2]=normalizeML((float)pFingerADC[2],220.0,208.0);
+  
+  ANNRun();
+
+  rt=GetANNMaxIndex(); 
+  
+  return rt;
+}
 
 void setMotorGesture(int gesture_number)
 {
@@ -253,4 +368,42 @@ void SevenSEG(byte DECNumber)
   numberShift=PORTD&B11100011;
   numberShift=numberShift|(DECNumber<<2);
   PORTD=numberShift;
+}
+
+float HardSigmoid(float x0)
+{
+    float retHS=0.0;
+    
+    if(x0 < -2.5)
+    {
+        retHS=0.0;
+    }
+    else if(x0 > 2.5)
+    {
+        retHS=1.0;
+    }
+    else if((-2.5 <= x0)&&(x0 <= 2.5))
+    {
+        retHS=0.2 * x0 + 0.5;
+    }
+
+    return retHS;
+}
+
+float normalizeML(float x0,float MaxV,float MinV)
+{
+  float rtNormal=0.0;
+  if(MaxV<=x0)
+  {
+    rtNormal= 1.0;
+  }
+  else if(MinV>=x0)
+  {
+    rtNormal=0.0;
+  }
+  else
+  {
+    rtNormal=(x0-MinV)/(MaxV-MinV);
+  }
+  return rtNormal;
 }
