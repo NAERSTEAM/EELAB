6e475c6118bab2086d91c5446ecc6bbf0d940517
unknown <tikao@mail.naer.edu.tw>
2020-05-30 11:24:47 +0800

    Added "LAB_20200530_0_hand sign recognition"

---
+++ LAB_20200530_0_HandSignRecognition/LAB_20200530_0_handsignrecognition.py
@@ -0,0 +1,24 @@
+#Cution:This example is not supported by the officail ImageAI!!!
+#This is the examplpe for AIT test "Live window"
+
+
+from imageai.Detection.Custom import CustomVideoObjectDetection
+import os
+import cv2
+
+execution_path = os.getcwd()
+camera = cv2.VideoCapture(0)
+
+video_detector = CustomVideoObjectDetection()
+video_detector.setModelTypeAsYOLOv3()
+video_detector.setModelPath("detection_model-ex-012--loss-0003.960.h5")
+video_detector.setJsonPath("detection_config HandSign.json")
+video_detector.loadModel(detection_speed="fastest")
+
+video_detector.detectObjectsFromVideo(camera_input=camera,
+                                          output_file_path=os.path.join(execution_path, "HandSignRecognition"),
+                                          frames_per_second=20,
+                                          minimum_percentage_probability=80,
+                                          log_progress=True,
+                                          save_detected_video=False,
+                                          live_window=True)
